eb5e2d00eda1fe1de20c497cfce56ccdef419229
Alex Hudson <hudsonj.alex@gmail.com>
2021-08-24 12:51:34 +0100

    added header, logo and buttons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import logo from "./images/logo.svg";
 
 function App() {
   return (
     <div className="App">
-      <div className="App-border-header" />
+      <div className="App-border-header">
+        <img src={logo} className={"acme-logo"} alt="ACME corp"></img>
+        <div className={"home-btn"}>Home</div>
+        <div className={"about-btn"}>About Us</div>
+      </div>
     </div>
   );
 }
